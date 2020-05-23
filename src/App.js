a6363f40869a45e0fb1ff345ecc887d1dc274cf4
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-23 17:28:23 -0300

    useEffect called to dump n read from localStorage.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  /* DidUpdate */
+  useEffect(() => {
+    if (techs.length > 0) {
+      localStorage.setItem('techs', JSON.stringify(techs));
+    }
+  }, [techs]);
+
+  /* DidMount */
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs));
+    }
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {techs.map((tech) => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={(e) => setNewTech(e.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() => {
+          setTechs([...techs, newTech]);
+          setNewTech('');
+        }}
+      >
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
