5d7dd7b2cd4d6d79712a35e38c6d380ced371cba
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-23 15:56:08 -0300

    Revert "React-hooks eslint config."
    This reverts commit 94d54beb56f6572bf92ebf194573e3430bc1c9ee.

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,9 +32,5 @@ module.exports = {
       { extensions: [ '.jsx', '.js']},
     ],
     'import/prefer-default-export': 'off',
-    'no-param-reassign': 'off',
-    'no-console': [ 'error', {allow: ['tron']}],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
   },
 };
