94d54beb56f6572bf92ebf194573e3430bc1c9ee
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-23 15:55:08 -0300

    React-hooks eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,5 +32,9 @@ module.exports = {
       { extensions: [ '.jsx', '.js']},
     ],
     'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': [ 'error', {allow: ['tron']}],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
